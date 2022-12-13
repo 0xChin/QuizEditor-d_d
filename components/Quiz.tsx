f42332930541e4a20d81626a1cf172b6bffc204a
AlanRacciatti <alanracciatti1220@gmail.com>
2022-12-13 00:32:42 -0300

    feat: add question creation

--- components/Quiz.tsx
+++ components/Quiz.tsx
@@ -1,5 +1,7 @@
 import { Box, VStack, Text, Button, Input } from "@chakra-ui/react";
 import React, { Dispatch, FC, useState, SetStateAction } from "react";
+import styles from "../styles/Quiz.module.css";
+import { FaTrash, FaPlus } from "react-icons/fa";
 
 interface QuizProps {
   quiz: string;
@@ -44,14 +46,18 @@ const Quiz: FC<QuizProps> = (props: QuizProps) => {
   };
 
   const setCorrectAnswer = (answerIndex: number) => {
+    if (!quiz.questions[currentQuestionIndex].options[answerIndex]) return;
+
     const newQuiz = quiz;
     const previousCorrectAnswerIndex = newQuiz.questions[
       currentQuestionIndex
     ].options.findIndex((o) => o.correct);
 
-    newQuiz.questions[currentQuestionIndex].options[
-      previousCorrectAnswerIndex
-    ].correct = false;
+    if (previousCorrectAnswerIndex !== -1) {
+      delete newQuiz.questions[currentQuestionIndex].options[
+        previousCorrectAnswerIndex
+      ].correct;
+    }
 
     newQuiz.questions[currentQuestionIndex].options[answerIndex].correct = true;
 
@@ -66,6 +72,15 @@ const Quiz: FC<QuizProps> = (props: QuizProps) => {
     props.setCode(JSON.stringify(newQuiz, null, 2));
   };
 
+  const changeAnswer = (v, optionIndex) => {
+    const newQuiz = quiz;
+
+    newQuiz.questions[currentQuestionIndex].options[optionIndex].answer =
+      v.target.value;
+
+    props.setCode(JSON.stringify(newQuiz, null, 2));
+  };
+
   const getQuestionBackground = (optionIndex: number) => {
     if (quiz.questions[currentQuestionIndex].options[optionIndex].correct) {
       return "green.400";
@@ -73,17 +88,83 @@ const Quiz: FC<QuizProps> = (props: QuizProps) => {
     return "gray.600";
   };
 
+  const deleteAnswer = () => {
+    if (quiz.questions.length === 1) {
+      return alert("Don't leave me without questions :(");
+    }
+
+    let newQuiz = quiz;
+
+    const shouldGoToPreviousQuestion =
+      currentQuestionIndex + 1 === quiz.questions.length;
+
+    if (shouldGoToPreviousQuestion) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
+    }
+
+    newQuiz.questions.splice(currentQuestionIndex, 1);
+
+    props.setCode(JSON.stringify(newQuiz, null, 2));
+  };
+
+  const deleteOption = (optionIndex) => {
+    if (quiz.questions[currentQuestionIndex].options.length === 1) {
+      return alert("Don't leave me without options :(");
+    }
+
+    let newQuiz = quiz;
+
+    newQuiz.questions[currentQuestionIndex].options.splice(optionIndex, 1);
+
+    props.setCode(JSON.stringify(newQuiz, null, 2));
+  };
+
+  const addOption = () => {
+    let newQuiz = quiz;
+
+    newQuiz.questions[currentQuestionIndex].options.push({
+      answer: "Introduce your option here :)",
+    });
+
+    props.setCode(JSON.stringify(newQuiz, null, 2));
+  };
+
+  const addQuestion = () => {
+    let newQuiz = quiz;
+
+    newQuiz.questions.push({
+      question: "Add your question here :)",
+      options: [
+        {
+          answer: "Your first option here fren :)",
+        },
+      ],
+    });
+
+    props.setCode(JSON.stringify(newQuiz, null, 2));
+
+    setCurrentQuestionIndex(newQuiz.questions.length - 1);
+  };
+
   return (
     <>
       <VStack spacing={4} background="gray.900" padding="6" borderRadius="md">
-        <Text fontWeight="bold" w="100%">
+        <Box display="flex" w="100%" justifyContent="space-between">
           <Input
             type="text"
             value={quiz.questions[currentQuestionIndex].question}
-            w="100%"
+            w="80%"
             onChange={changeQuestion}
           />
-        </Text>
+          <Button
+            className={styles.trash}
+            background="red.600"
+            onClick={deleteAnswer}
+          >
+            Delete
+            <FaTrash className={styles.trashIcon} />
+          </Button>
+        </Box>
         {quiz.questions[currentQuestionIndex].options.map((o, index) => {
           return (
             <Box
@@ -94,11 +175,41 @@ const Quiz: FC<QuizProps> = (props: QuizProps) => {
               cursor="pointer"
               onClick={() => setCorrectAnswer(index)}
               key={index}
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
             >
-              {o.answer}
+              <Input
+                value={o.answer}
+                w="70%"
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+                onChange={() => changeAnswer(event, index)}
+              />
+              <Button
+                className={styles.trash}
+                background="red.600"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  deleteOption(index);
+                }}
+                size="sm"
+                marginBottom="5px"
+              >
+                <FaTrash className={styles.trashIcon} />
+              </Button>
             </Box>
           );
         })}
+        <Button
+          className={styles.trash}
+          colorScheme="yellow"
+          backgroundColor="yellow.600"
+          onClick={addOption}
+        >
+          <FaPlus className={styles.trashIcon} color="white" />
+        </Button>
         <Box
           display="flex"
           justifyContent="space-between"
@@ -108,7 +219,7 @@ const Quiz: FC<QuizProps> = (props: QuizProps) => {
           <Text w="100%">{`Question ${currentQuestionIndex + 1}/${
             quiz.questions.length
           }`}</Text>
-          <Box w="100%" display="flex">
+          <Box w="100%" display="flex" justifyContent="space-between">
             <Button
               mx="2"
               visibility={previousButtonVisibility()}
@@ -119,6 +230,17 @@ const Quiz: FC<QuizProps> = (props: QuizProps) => {
             <Button visibility={nextButtonVisibility()} onClick={nextQuestion}>
               {"Next >"}
             </Button>
+            <Box onClick={addQuestion}>
+              <Button
+                className={styles.trash}
+                colorScheme="yellow"
+                backgroundColor="yellow.600"
+                color="white"
+              >
+                {"New"}
+                <FaPlus className={styles.trashIcon} color="white" />
+              </Button>
+            </Box>
           </Box>
         </Box>
       </VStack>
